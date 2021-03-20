05b28ffa8fd0440a35b8048f49ddf91e099ce979
Mikołaj Wargowski <mikoajw20@gmail.com>
2021-03-20 08:52:03 +0100

    ✨ Account Subscription Type VO

--- src/errors/app.error.ts
+++ src/errors/app.error.ts
@@ -1,5 +1,5 @@
 export class AppError extends Error {
-  constructor(message: string, public readonly name = 'AppError') {
+  constructor(message: string, public readonly name = 'AppError', public readonly errorCode = 500) {
     super(message);
 
     Error.captureStackTrace(this, AppError.captureStackTrace);
--- src/modules/platform-access/core/account/account.aggregate-root.spec.ts
+++ src/modules/platform-access/core/account/account.aggregate-root.spec.ts
@@ -2,6 +2,7 @@ import { UniqueEntityID } from '@root/shared/unique-entity-id';
 import { AccountEmail } from '../account-email/account-email.value-object';
 import { AccountPassword } from '../account-password/account-password.value-object';
 import { AccountStatus } from '../account-status/account-status.value-object';
+import { SubscriptionType } from '../subscription-type/subscription-type.value-object';
 import { Account } from './account.aggregate-root';
 import { UserLoggedInEvent } from './events/user-logged-in.domain-event';
 
@@ -14,6 +15,7 @@ describe('[DOMAIN] Platform Access/Account', () => {
         password,
         email: AccountEmail.fromPersistence('john@doe.com'),
         status: AccountStatus.Confirmed,
+        subscriptionType: null,
       },
       new UniqueEntityID(),
     );
@@ -31,6 +33,7 @@ describe('[DOMAIN] Platform Access/Account', () => {
         password,
         email: AccountEmail.fromPersistence('john@doe.com'),
         status: AccountStatus.WaitingForConfirmation,
+        subscriptionType: null,
       },
       new UniqueEntityID(),
     );
@@ -48,6 +51,7 @@ describe('[DOMAIN] Platform Access/Account', () => {
         password,
         email: AccountEmail.fromPersistence('john@doe.com'),
         status: AccountStatus.Confirmed,
+        subscriptionType: null,
       },
       new UniqueEntityID(),
     );
@@ -55,5 +59,77 @@ describe('[DOMAIN] Platform Access/Account', () => {
     await account.login('test123');
 
     expect(account.getDomainEvents()[0] instanceof UserLoggedInEvent).toBeTruthy();
+  });
+
+  test('should throw an error if subscription is already set', async () => {
+    const password = await AccountPassword.createNew('test123');
+
+    const account = Account.fromPersistence(
+      {
+        password,
+        email: AccountEmail.fromPersistence('john@doe.com'),
+        status: AccountStatus.Confirmed,
+        subscriptionType: SubscriptionType.PaidMembership,
+      },
+      new UniqueEntityID(),
+    );
+
+    expect(() => account.assignFreeMembership()).toThrowError(
+      'Cannot assign new subscription. Subscription is already set.',
+    );
+  });
+
+  test('should assign free membership', async () => {
+    const password = await AccountPassword.createNew('test123');
+
+    const account = Account.fromPersistence(
+      {
+        password,
+        email: AccountEmail.fromPersistence('john@doe.com'),
+        status: AccountStatus.Confirmed,
+        subscriptionType: null,
+      },
+      new UniqueEntityID(),
+    );
+
+    account.assignFreeMembership();
+
+    expect(account.getSubscriptionType().equals(SubscriptionType.Free)).toBeTruthy();
+  });
+
+  test('should throw an error if subscription is already paid', async () => {
+    const password = await AccountPassword.createNew('test123');
+
+    const account = Account.fromPersistence(
+      {
+        password,
+        email: AccountEmail.fromPersistence('john@doe.com'),
+        status: AccountStatus.Confirmed,
+        subscriptionType: SubscriptionType.PaidMembership,
+      },
+      new UniqueEntityID(),
+    );
+
+    expect(() => account.assignPaidMembership()).toThrowError(
+      'Cannot assign paid membership. Subscription is already paid.',
+    );
+  });
+
+  test('should assign paid membership', async () => {
+    const password = await AccountPassword.createNew('test123');
+
+    const account = Account.fromPersistence(
+      {
+        password,
+        email: AccountEmail.fromPersistence('john@doe.com'),
+        status: AccountStatus.Confirmed,
+        subscriptionType: SubscriptionType.Free,
+      },
+      new UniqueEntityID(),
+    );
+
+    account.assignPaidMembership();
+
+    expect(account.getSubscriptionType().equals(SubscriptionType.PaidMembership)).toBeTruthy();
   });
 });
